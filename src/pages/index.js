2d0e61fedb14ac626ed56321cab8576a21373a95
Marianna Melchanova <melchanovamarianna@yandex.ru>
2023-04-07 21:46:55 +0300

    8пр

---
+++ src/pages/index.js
@@ -0,0 +1,89 @@
+import "./index.css";
+
+import {Card} from "../scripts/components/Cards.js";
+import {initialCards} from "../scripts/components/iniitialCards.js";
+import {FormValidator} from "../scripts/components/FormValidator.js";
+import {UserInfo} from "../scripts/components/UserInfo.js";
+import {Section} from "../scripts/components/Section.js";
+import {PopupWithForm} from "../scripts/components/PopupWithForm.js";
+import {PopupWithImage} from "../scripts/components/PopupWithImage.js";
+
+const editButtonOpen = document.querySelector(".profile__edit-button");
+const profilePopup = document.querySelector(".popup_edit");
+const profilePopupForm = document.querySelector(".popup__form_edit");
+const profilePopupTitle = document.querySelector(".popup__input_value_name");
+const profilePopupSubtitle = document.querySelector(".popup__input_value_about");
+
+const popupAdd = document.querySelector(".popup_add");
+const openAddButton = document.querySelector(".profile__add-button");
+const popupFormAdd = document.querySelector(".popup__form_add");
+
+const elementsCards = document.querySelector(".elements__cards");
+
+const imagePopup = document.querySelector(".image-popup");
+
+const validationConfig = {
+    formSelector: ".popup__form",
+    inputSelector: ".popup__input",
+    inputErrorClass: "popup__input_type_error",
+    submitButtonSelector: ".popup__button",
+    inactiveButtonClass: "popup__button_disabled",
+    errorClass: "error_active",
+};
+const validationEdit = new FormValidator(validationConfig, profilePopupForm);
+validationEdit.enableValidation();
+const validationAdd = new FormValidator(validationConfig, popupFormAdd);
+validationAdd.enableValidation();
+
+const cardOpenPopup = new PopupWithImage(imagePopup);
+
+const userInfo = new UserInfo({
+    nameSelector: ".profile__title",
+    aboutSelector: ".profile__subtitle",
+});
+
+function createCard(item) {
+    const card = new Card(item, ".template", () => {
+        cardOpenPopup.open(item);
+    });
+
+    return card.generateCard();
+}
+
+const formEdit = new PopupWithForm(profilePopup, function (element) {
+    userInfo.setUserInfo(element);
+});
+
+const formAdd = new PopupWithForm(popupAdd, function ({ name, link }) {
+    cardSection.addItem(
+        createCard({
+            name: name,
+            link: link,
+        })
+    );
+});
+
+editButtonOpen.addEventListener("click", () => {
+    formEdit.open();
+    const { name, about } = userInfo.getUserInfo();
+    profilePopupTitle.value = name;
+    profilePopupSubtitle.value = about;
+});
+
+openAddButton.addEventListener("click", () => {
+    formAdd.open();
+    validationAdd.toggleButtonState();
+});
+
+const cardSection = new Section({
+    items: initialCards,
+    renderer: (data) => {
+        cardSection.addItem(createCard(data));
+    },
+    container: elementsCards,
+});
+cardSection.renderElements();
+
+formEdit.setEventListener();
+formAdd.setEventListener();
+cardOpenPopup.setEventListener();
---
+++ src/scripts/components/Cards.js
@@ -0,0 +1,48 @@
+class Card {
+    constructor(data, templateSelector, handleNewClick) {
+        this._name = data.name;
+        this._link = data.link;
+        this._templateSelector = templateSelector;
+        this._handleNewClick = handleNewClick;
+    }
+
+    _getTemplate() {
+        const cardElement = document.querySelector(this._templateSelector).content.querySelector(".article").cloneNode(true);
+
+        return cardElement;
+    }
+    generateCard() {
+        this._element = this._getTemplate();
+        this._setEventListeners();
+
+        this._element.querySelector(".article__item").src = this._link;
+        this._element.querySelector(".article__item").alt = this._name;
+        this._element.querySelector(".article__title").textContent = this._name;
+        this._buttonIconToggle = this._element.querySelector(".article__icon");
+        this._popupCardDelete = this._element.querySelector(".article__delete");
+
+        return this._element;
+    }
+    
+    _handleCardLike = () => {
+        this._buttonIconToggle.classList.toggle("article__like-active");
+    };
+    _handledeleteCard = () => {
+        this._element.remove();
+        this._element = null;
+    };
+
+    _setEventListeners() {
+        this._element.querySelector(".article__item").addEventListener("click", () => {
+            this._handleNewClick(this._name, this._link);
+        });
+        this._element.querySelector(".article__icon").addEventListener("click", () => {
+            this._handleCardLike();
+        });
+
+        this._element.querySelector(".article__delete").addEventListener("click", () => {
+            this._handledeleteCard();
+        });
+    }
+}
+export {Card};
